4ecc088baefc031f32242f6801a786f46637fe35
darkwing-09 <darkwing.ananta@gmail.com>
2026-03-13 15:48:21 +0000

    update: improve NLP text preprocessing

--- NLP /text_processing.py
+++ NLP /text_processing.py
@@ -1,4 +1,5 @@
 import re
+import pandas as pd
 def html_tag_rem(text):
     pattern=re.compile('<.*?>')
     return pattern.sub(r'',text)
@@ -15,3 +16,7 @@ p=""" <html> <!DOCTYPE html> <html>
 
 print(html_tag_rem(p))
 
+##When you use any data set let say df=pd.read_csv('data.csv')
+##then you can apply this function to a column of the data set like this
+df=pd.read_csv('data.csv') # reading the data set using pandas
+df['column_name']=df['column_name'].apply(html_tag_rem) # where df is the file which is being read by pandas and column_name is the name of the column which contains the text data which you want to clean from html tags.
