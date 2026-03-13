9e3b8dbd6263996fd5e53371954b0f75a240a4f9
darkwing-09 <darkwing.ananta@gmail.com>
2026-03-13 15:32:25 +0000

    feat: add NLP text preprocessing implementation

---
+++ NLP /text_processing.py
@@ -0,0 +1,19 @@
+import re
+def html_tag_rem(text):
+    pattern=re.compile('<.*?>')
+    return pattern.sub(r'',text)
+
+p=""" <html> <!DOCTYPE html> <html>
+<body>
+
+<p>This is <b>bold</b> text.</p>
+<p>This is <i>italic</i> text.</p>
+<p>This is <u>underlined</u> text.</p>
+<p>This is <strong>important</strong> text.</p>
+<p>This is <em>emphasized</em> text.</p>
+
+</body>
+</html>"""
+
+print(html_tag_rem(p))
+
