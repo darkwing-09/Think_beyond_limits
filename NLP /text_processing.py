eca7b1109d4032eb6f5af4574b3b08e8f1a36d1b
darkwing-09 <darkwing.ananta@gmail.com>
2026-03-13 21:03:46 +0530

    Fix formatting issues in text_processing.py

--- NLP /text_processing.py
+++ NLP /text_processing.py
@@ -5,13 +5,11 @@ def html_tag_rem(text):
 
 p=""" <html> <!DOCTYPE html> <html>
 <body>
-
 <p>This is <b>bold</b> text.</p>
 <p>This is <i>italic</i> text.</p>
 <p>This is <u>underlined</u> text.</p>
 <p>This is <strong>important</strong> text.</p>
 <p>This is <em>emphasized</em> text.</p>
-
 </body>
 </html>"""
 
