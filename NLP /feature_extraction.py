6822029cf0b2a1c85b888e19c15e1d13e2a30591
darkwing-09 <darkwing.ananta@gmail.com>
2026-03-17 17:59:26 +0000

    Common Words

--- NLP /feature_extraction.py
+++ NLP /feature_extraction.py
@@ -2,3 +2,8 @@
 #Better the feature better the input
 #Here We'll change text into numbers and it's one of the difficult processess if we compare to feature extraction of image or audio-clip
 
+#Common words Which we'll follow up are - 
+# Corpus - the concatenation of all words from all the dataset { e.g If dataset have 100 col , the the corpus is adding the each column text and concatenate in to make a big string }
+#Vocab - Its the unique words the corpus is made up of 
+#Document - Each column is a document , i.e each unique representation is document 
+#word - each token in the corpus is word
