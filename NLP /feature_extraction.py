7194ff99e25e54468b017925842d318d8a62b833
darkwing-09 <darkwing.ananta@gmail.com>
2026-03-17 18:07:38 +0000

    New

--- NLP /feature_extraction.py
+++ NLP /feature_extraction.py
@@ -7,3 +7,34 @@
 #Vocab - Its the unique words the corpus is made up of 
 #Document - Each column is a document , i.e each unique representation is document 
 #word - each token in the corpus is word
+
+
+#eg
+"""
+doc_id,document
+1,"I love learning NLP"
+2,"This system is very slow"
+3,"Book a bus ticket for Delhi"
+4,"Where is my order now"
+5,"Cancel my booking immediately"
+6,"The service was excellent"
+7,"I am not happy with this product"
+8,"Track my bus location"
+9,"How to reset my password"
+10,"The app crashes frequently"
+11,"Show available buses to Lucknow"
+12,"Payment was successful"
+13,"Refund my money"
+14,"Login is not working"
+15,"I want to change my seat"
+"""
+
+#corpus - I love learning NLP This system is very slow Book a bus ticket for Delhi Where is my order now Cancel my booking immediately The service was excellent I am not happy with this product Track my bus location How to reset my password The app crashes frequently Show available buses to Lucknow Payment was successful Refund my money Login is not working I want to change my seat
+"""vocab -['a','am','app','available','book','booking','bus','buses','cancel','change',
+'crashes','delhi','excellent','for','frequently','happy','how','i','immediately',
+'is','learning','location','login','love','lucknow','money','my','nlp','not',
+'now','order','password','payment','product','refund','reset','seat','service',
+'show','slow','successful','system','the','this','ticket','to','track','very',
+'was','want','where','with','working']"""
+#words = 52
+
