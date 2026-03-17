8e42190607e6e57fef5c04cce69cc4d970f7f19f
darkwing-09 <darkwing.ananta@gmail.com>
2026-03-17 17:51:18 +0000

    Intro

--- NLP /feature_extraction.py
+++ NLP /feature_extraction.py
@@ -1,1 +1,4 @@
+#Garbage In -> Garbage Out , bad algo can better results with better input and vice versa
+#Better the feature better the input
+#Here We'll change text into numbers and it's one of the difficult processess if we compare to feature extraction of image or audio-clip
 
