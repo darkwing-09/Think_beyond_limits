264ea00ebaba31959773cd8e934af3cb3c0a2ab4
darkwing-09 <darkwing.ananta@gmail.com>
2026-03-17 19:56:45 +0000

    BOW

--- NLP /feature_extraction.py
+++ NLP /feature_extraction.py
@@ -38,4 +38,59 @@ doc_id,document
 'was','want','where','with','working']"""
 #words = 52
 
-##One Hot Coding
+##One Hot EnCoding
+
+#It produces the sparse matrix and the order of matrix is not fixed , and to ML models the fix input is always given
+#e.g
+
+# I1=I am Varun Dev                             I am Varun Dev studying NLP works on
+# I2=I am studying NLP
+# I3=Varun works on NLP
+ # I1=[[1,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0],[0,0,1,0,0,0,0,0],[0,0,0,1,0,0,0,0]] , I2=[[1,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0],[0,0,0,0,1,0,0,0],[0,0,0,0,0,1,0,0]] , I3=[[0,0,1,0,0,0,0,0],[0,0,0,0,0,0,1,0],[0,0,0,0,0,0,0,1],[0,0,0,0,0,1,0,0]]
+# And if any how any input as I4 came in as = I am varun dev mishra , them the order will be 5*8 and mishra is not in words , then the model will be more confused rater training
+
+
+#SO THE PROMBLEMS ARE -
+#SPARSITY
+#OOV- OUT OF VOCAB
+# NO FIXED SIZE 
+#NO CAPTURING OF SEMENTIC MEANING
+
+
+
+#2 - BAG OF WORDS
+"""
+What it does:
+
+Takes text
+
+Breaks it into words
+
+Builds a vocabulary
+
+Counts how many times each word appears
+""" 
+#IT TELLS THAT THE OCCURENCE OF WORS I THE DOCUMENT AND REST IS SAME AS ONE HOT ENCODING
+# I1=I am Varun                             I am Varun is loves Dev
+# I2=Varun is Varun
+# I3=Varun loves Dev
+ # I1=[1,1,1,0,0] , I2=[0,0,2,1,0], I3=[0,0,1,0,1,1]
+#CONTEXT AND SEMENTIC MEANING IS MEANT
+
+# uRl = https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
+#must go 
+#class sklearn.feature_extraction.text.CountVectorizer(*, input='content', encoding='utf-8', decode_error='strict', strip_accents=None, lowercase=True, preprocessor=None, tokenizer=None, stop_words=None, token_pattern='(?u)\\b\\w\\w+\\b', ngram_range=(1, 1), analyzer='word', max_df=1.0, min_df=1, max_features=None, vocabulary=None, binary=False, dtype=<class 'numpy.int64'>)
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('/content/NLP_DATA - Sheet1(1).csv')
+print(df)
+df = df.dropna(axis=1, how='all')
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer()
+bow=cv.fit_transform(df['text'])
+print(cv.vocabulary_)   # Removes stopword
+#Array 
+print(bow[0].toarray())
+print(bow[1].toarray())
+print(bow[2].toarray())
