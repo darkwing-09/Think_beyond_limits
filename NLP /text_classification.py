7125119ce25d476084bfc7e1406cb87c8806bf08
darkwing-09 <darkwing.ananta@gmail.com>
2026-03-30 16:58:29 +0000

    Api

--- NLP /text_classification.py
+++ NLP /text_classification.py
@@ -11,3 +11,6 @@
 # Modelling 
 #ML- Naive , Random Forest
 #DL - RNN(LSTM) , CNN , BERT
+
+#Heuristic Approach - When the data is not enough to train model, thus some jugaadu approach is followed 
+#Api - https://nlpcloud.com/home/playground/headline-generation  grenerate Api from here for your work case and use them in your model to genrate the results desired. 
