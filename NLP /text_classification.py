fe822c5f5f02e89b5e25570f2c7412b40d17f684
darkwing-09 <darkwing.ananta@gmail.com>
2026-03-30 18:28:12 +0000

    Sateset

--- NLP /text_classification.py
+++ NLP /text_classification.py
@@ -13,4 +13,6 @@
 #DL - RNN(LSTM) , CNN , BERT
 
 #Heuristic Approach - When the data is not enough to train model, thus some jugaadu approach is followed 
-#Api - https://nlpcloud.com/home/playground/headline-generation  grenerate Api from here for your work case and use them in your model to genrate the results desired. 
+#Api - https://nlpcloud.com/home/playground/headline-generation , GCP , AWS  generate Api from here for your work case and use them in your model to genrate the results desired. 
+
+# download th4e datset from https://www.kaggle.com/datasets/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews
